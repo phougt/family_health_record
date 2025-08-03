937a7d79026fc45daa49837897497f6663090fcb
Mearaphoumint Mon <74492753+phougt@users.noreply.github.com>
2025-08-03 20:37:54 +0700

    Update ApiHelper::successResponse logic

--- app/helpers/ApiHelper.php
+++ app/helpers/ApiHelper.php
@@ -1,10 +1,28 @@
 <?php
 namespace App\Helpers;
 
+use Illuminate\Pagination\LengthAwarePaginator;
+
 class ApiHelper
 {
     public static function successResponse($data, $message = 'Success', $code = 200)
     {
+        if ($data instanceof LengthAwarePaginator) {
+            return response()->json([
+                'message' => $message,
+                'data' => $data->items(),
+                'pagination' => [
+                    'total' => $data->total(),
+                    'count' => $data->count(),
+                    'per_page' => $data->perPage(),
+                    'current_page' => $data->currentPage(),
+                    'last_page' => $data->lastPage(),
+                    'next_page_url' => $data->nextPageUrl(),
+                    'prev_page_url' => $data->previousPageUrl(),
+                ]
+            ], $code);
+        }
+
         return response()->json([
             'message' => $message,
             'data' => $data,
